e8caf37db45e34088df724ea1d86589e12d99635
duytpm16 <39827376+duytpm16@users.noreply.github.com>
2018-08-24 10:20:35 -0400

    Update qtl2_scan1.R

--- qtl2_scan1.R
+++ qtl2_scan1.R
@@ -1,15 +1,24 @@
-### This script reads in the required data file to run the qtl2 scan1 function.
-#   The proteins can be ran in 'chunks' or all at once.
-#     Example for chunk size/number: 
-#         There should be 5433 protein columns. If chunk size is 1000, then there should be 6 scan1 runs, 
-#         with the chunk_number value being 1-6 to get the column numbers:
-#         1-1000,1001-2000,2001-3000,3001-4000,4001-5000,5001-5433, respectively.
+### This script reads in the required input data file generated from gather_qtl2_scan1_input_data.R
+#       to run the qtl2 scan1 function.
+#   The QTL scan can be ran in 'chunks' or all at once.
+#       Example for chunk size/number: 
+#           Suppose there are 5433 phenotype columns. If chunk size is 1000, then there should be 6 different chunks
+#             of scan1 runs, with the chunk_number value being 1-6 to get the column numbers:
+#             1-1000,1001-2000,2001-3000,3001-4000,4001-5000,5001-5433, respectively.
 #
-#         If you do not want to run in chunks, set chunk_size to 5433 and chunk_num to 1.
+#           If you do not want to run in chunks, set use_chunks to FALSE.
 #
-### Input: qtl2 data file generated from:
+### Input:
+#       1: input.file:    Path + prefix to the qtl2 input data generated from gather_qtl2_scan1_input_data.R
+#       2: num_cores:     Number of cores to run
+#       3: should_rankz:  Logical value to use the rankz dataset instead of normalized
+#       4: use_sexint:    Logical value to use sex as an interaction term
+#       5: use_chunks:    Logical value to run QTL scans in chunks
+#       6: chunk_number:  Numeric value of the chunk number. Not needed if use_chunks is FALSE
+#       7: chunk_size:    Numeric value of chunk size. Should be consistent. Not needed if use_chunks is FALSE
 #
-### Output: Dataframe of the LOD scoress for each of the proteins that was given to scan1.
+### Output: 
+#       1: Matrix containing LOD scoress for each of the phenotype that was given to scan1 at each marker.
 #
 ### Author: Duy Pham, 'phenotype range run' was taken from Dan Gatti
 ### Date:   July 10, 2018
@@ -32,32 +41,32 @@ library(RSQLite)
 
 
 
-### Command line arguments.
-# 1: input.file:    Prefix of qtl2 input data
-# 3: num_cores:     Number of cores to run
-# 4: should.rankz:  Logical value to use the rankz dataset instead of normalized
+### Command line arguments / Variables to change
+# 1: input.file:    Path + prefix to the qtl2 input data generated from gather_qtl2_scan1_input_data.R
+# 2: num_cores:     Number of cores to run
+# 3: should_rankz:  Logical value to use the rankz dataset instead of normalized
+# 4: use_int:       Logical value to use an interaction term
+# 5: use_chunks:    Logical value to run QTL scans in chunks
+# 6: int_name:      Name of the interaction term
+# 7: chunk_number:  Numeric value of the chunk number. Not needed if use_chunks is FALSE
+# 8: chunk_size:    Numeric value of chunk size. Should be consistent. Not needed if use_chunks is FALSE
 args = commandArgs(trailingOnly = TRUE)
 
 load(paste0(args[1],"_qtl2_input.RData"))
 num_cores <- as.numeric(args[2])
 should.rankz <- as.logical(args[3])
-use.diff <- as.logical(args[4])
+use_sexint <- as.logical(args[4])
 use_chunks <- as.logical(args[5])
-
-if(use_chunks){
-  chunk_number <- as.numeric(args[6])
-  chunk_size <- as.numeric(args[7])
-}
 
 
 ### Check to see if required data are loaded
-stopifnot(c("genoprobs", "K", "map", "markers", "covar", 
+stopifnot(c("norm","pheno.dict","display.name","genoprobs", "K", "map", "markers", "covar", 
             "covar.factors", "samples", "rankz", "raw","datatype") %in% ls())
 
 
 
-### If should.rankz is true used the normalized rankz dataset instead, else use the normalized dataset.
-if(should.rankz){
+### If should_rankz is true used the normalized rankz dataset, else use the normalized dataset.
+if(should_rankz){
   
   expr <- rankz
   
@@ -67,17 +76,31 @@ if(should.rankz){
   
 }
 
-if(use.diff){
- int_covar_name <- args[5]
- formula <- as.formula(paste0('~', int_covar_name))
+
+### If sex should be used as an interaction term
+if(use_int){
+  
+ int_name <- args[6]
+ formula <- as.formula(paste0('~', int_name))
  int_mat <- model.matrix(formula, data = samples)[,-1]
+  
 }else{
+  
  int_mat <- NULL
+  
 }
 
+### If running qtl scans by chunks, get chunk size and chunk number
+if(use_chunks){
+  chunk_number <- as.numeric(args[7])
+  chunk_size <- as.numeric(args[8])
+}
+
+
+### Running QTL2 scan1 function
 if(use_chunks == FALSE){
   
-    ### Qtl2 scan1 run without chunks
+    ### qtl2 scan1 without chunks
     qtl <- scan1(genoprobs = genoprobs, 
                  pheno = expr,
                  kinship = K, 
@@ -88,14 +111,14 @@ if(use_chunks == FALSE){
     class(qtl) = c("scan1", "matrix")
 
     ### Save output of scan1 to current directory
-    if(use.diff){
-       if(should.rankz){
-          saveRDS(qtl, file = paste0(args[1], "_sexint_rZ_qtl_lod.rds"))
+    if(use_int){
+       if(should_rankz){
+          saveRDS(qtl, file = paste0(args[1], "_",int_name,"_int_rZ_qtl_lod.rds"))
        }else{
-          saveRDS(qtl, file = paste0(args[1], "_sexint_qtl_lod.rds"))
+          saveRDS(qtl, file = paste0(args[1], "_",int_name,"_int_norm_qtl_lod.rds"))
        }
     }else{
-       if(should.rankz){
+       if(should_rankz){
           saveRDS(qtl, file = paste0(args[1], "_rZ_qtl_lod.rds"))
        }else{
           saveRDS(qtl, file = paste0(args[1], "_qtl_lod.rds"))
@@ -115,7 +138,7 @@ if(use_chunks == FALSE){
 
    print(paste("Mapping columns:", pheno.rng[1], "to", pheno.rng[length(pheno.rng)]))
 
-   # Run QTL scan with chunk
+   # Run QTL scan with chunks
    qtl <- scan1(genoprobs = genoprobs,
              pheno = expr[,pheno.rng,drop = FALSE],
              kinship = K,
@@ -124,17 +147,21 @@ if(use_chunks == FALSE){
              cores = num_cores)
 
    # Save the QTL chunk scan
-   if(use.diff){
-      if(should.rankz){
-         saveRDS(qtl, file = paste0(args[1], "_sexint_rZ_qtl_lod_",chunk_numer,".rds"))
+   if(use_int){
+     
+      if(should_rankz){
+         saveRDS(qtl, file = paste0(args[1], "_",int_name,"_int_rZ_qtl_lod_chunk_",chunk_numer,".rds"))
       }else{
-         saveRDS(qtl, file = paste0(args[1], "_sexint_qtl_lod_",chunk_number,".rds"))
+         saveRDS(qtl, file = paste0(args[1], "_",int_name,"_int_norm_qtl_lod_chunk_",chunk_number,".rds"))
       }
+     
    }else{
-      if(should.rankz){
-         saveRDS(qtl, file = paste0(args[1], "_rZ_qtl_lod_",chunk_number,".rds"))
+     
+      if(should_rankz){
+         saveRDS(qtl, file = paste0(args[1], "_rZ_qtl_lod_chunk_",chunk_number,".rds"))
       }else{
-         saveRDS(qtl, file = paste0(args[1], "_qtl_lod_",chunk_number,".rds"))
+         saveRDS(qtl, file = paste0(args[1], "_norm_qtl_lod_chunk_",chunk_number,".rds"))
       }
+     
    }		   
 }
