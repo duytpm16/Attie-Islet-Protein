2b038e2ebc9305f4c872ef4ba2fd1bd8868daa0a
duytpm16 <39827376+duytpm16@users.noreply.github.com>
2018-08-26 14:57:51 -0400

    Add files via upload

---
+++ qtl2_viewer_lipids.R
@@ -0,0 +1,59 @@
+#######################################################################################################################
+# This script generates dataset.* for QTL viewer the attie lipid data (cecum, liver, and plasma).
+#
+# Input:
+#    1.) Path to .RData file generated from qtl2_gather_scan1_input_data.R
+#    2.) Path to lod peaks .rds file generated from qtl2_findpeaks_scan1blup.R
+#    3.) Datatype as character. Should be 'phenotype' as required by QTL viewer.
+#    4.) Name for the dataset.* list.
+#
+# Output:
+#    1.) A .RData file containing the dataset.*, genoprobs, markers, map, and kinship matrix
+#
+# Author: Duy Pham
+# E-mail: duy.pham@jax.org
+# Date: July 31, 2018
+######################################################################################################################
+
+
+
+### Read in the required data
+#    1.) Path to .RData file generated from qtl2_gather_scan1_input_data.R
+#    2.) Path to lod peaks .rds file generated from qtl2_findpeaks_scan1blup.R
+#    3.) Datatype as character. Should be 'phenotype' as required by QTL viewer.
+#    4.) Name for the dataset.* list.
+load("~/Desktop/Attie/Lipids/Cecum_Lipids/attie_cecum_lipid_qtl2_input.Rdata")
+lod.peaks <- readRDS("~/Desktop/Attie/Lipids/Cecum_Lipids/attie_cecum_lipid_rZ_lod_peaks_6.rds")
+datatype <- 'phenotype'
+dataset_name <- 'dataset.cecum.lipids'
+
+
+
+### Formatiting the covar.factors 
+covar.factors <- data.frame(column.names = covar.factors)
+covar.factors$display.name <- gsub("([A-Z])([A-Z])", "\\1\\2 ", covar.factors$column.names)
+covar.factors$display.name <- gsub("(^|[[:space:]])([[:alpha:]])", "\\1\\U\\2", covar.factors$display.name, perl=TRUE)
+
+
+
+### Changing raw dataframe to matrix
+raw <- as.matrix(raw)
+
+
+
+### Creating the formatted dataset as required by QTL Viewer
+assign(dataset_name, list(annots = pheno.dict,
+                          covar = covar,
+                          covar.factors = covar.factors,
+                          datatype = datatype,
+                          display.name = display.name,
+                          lod.peaks = list(additive = lod.peaks),
+                          pheno = rankz,
+                          norm = norm,
+                          raw = raw,
+                          samples = samples))
+
+
+
+### Removing the data that is not stored in the dataset list
+rm(dataset_name, covar, covar.factors, datatype, display.name, lod.peaks, norm, pheno.dict, rankz, raw, samples)
