6de455112038bb4ffca9afd581fcdb68005751f9
duytpm16 <39827376+duytpm16@users.noreply.github.com>
2018-09-17 12:50:49 -0400

    Update and rename attie_islet_protein_284_normalization.r to attie_islet_protein_284_normalization.R

--- Islet Mediation/attie_islet_protein_284_normalization.R
+++ Islet Mediation/attie_islet_protein_284_normalization.R
@@ -1,4 +1,4 @@
-load("~/Desktop/Attie/Attie_rZ_QTL_Viewer_V3.RData")
+load("~/Desktop/Attie/Attie_rZ_QTL_Viewer_V2.RData")
 
 intersect.samples <- intersect(rownames(dataset.islet.rnaseq$expr), rownames(dataset.islet.proteins$expr))
 saveRDS(intersect.samples,'Attie_islet_intersect_samples.rds')
