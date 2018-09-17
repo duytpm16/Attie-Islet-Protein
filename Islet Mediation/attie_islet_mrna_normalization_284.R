26ff0c43a43c89a7ed34c251c3b85c06ea4dbc05
duytpm16 <39827376+duytpm16@users.noreply.github.com>
2018-09-17 15:11:35 -0400

    Update attie_islet_mrna_normalization_284.R

--- Islet Mediation/attie_islet_mrna_normalization_284.R
+++ Islet Mediation/attie_islet_mrna_normalization_284.R
@@ -1,9 +1,31 @@
+#################################################################################################################
+#  This script is used to re-normalize the attie islet mrna data based on samples that overlap with 
+#     attie islet protein.
+#
+#  Input:
+#    1.) .RData file that contains dataset.islet.proteins and dataset.islet.rnaseq (Attie_QTL_Viewer_V2.RData)
+#  
+#  Output:
+#    1.) .rds raw data
+#    2.) .rds normalized data by upper quantile
+#    3.) .rds rankz data 
+#    4.) .rds sample
+#
+#  Author: Duy Pham
+#  E-mail: duy.pham@jax.org
+#################################################################################################################
+
+### Load in Attie QTL viewer data
 load("~/Desktop/Attie/Attie_rZ_QTL_Viewer_V2.RData")
 
 
+
+### Find overlapping samples between islet protein and mrna datasets
 intersect.samples <- intersect(rownames(dataset.islet.rnaseq$expr), rownames(dataset.islet.proteins$expr))
 
 
+
+### Extract raw and samples data based on overlap data
 raw <- dataset.islet.rnaseq$raw[intersect.samples,]
 samples <- dataset.islet.rnaseq$samples[intersect.samples,]
 samples$DOwave <- as.factor(samples$DOwave)
@@ -11,6 +33,7 @@ samples$sex <- as.factor(samples$sex)
 
 
 
+### Prefix to save name of .rds data
 prefix <- 'attie_islet_mrna_284'
 
 raw_file <- paste0(prefix,"_filtered_raw.rds")
@@ -20,23 +43,22 @@ samples_file <-  paste0(prefix, "_samples_annot.rds")
 
 
 
+### Normalize raw data by upper quantile for each subject
 data.norm <- raw
-# Normalized read counts in each sample
 q <- apply(data.norm, 1, quantile, probs=0.75)
 data.norm <- data.norm/q                                  
 
 
 
 
-
-
-### Rank Z of normalized data.
+### Rank Z transformation function
 rankZ = function(x) {
   x = rank(x, na.last = "keep", ties.method = "average") / (sum(!is.na(x)) + 1)
   return(qnorm(x))
 } # rankZ()
 
 
+### Rankz across genes
 data.rz <- data.norm
 data.rz <- apply(data.rz, 2, rankZ)
 
