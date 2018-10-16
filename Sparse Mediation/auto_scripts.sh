2d098940fc1dcb9adeec5454bd059f67fbc3ee3d
duytpm16 <39827376+duytpm16@users.noreply.github.com>
2018-10-16 11:22:11 -0400

    Add files via upload

---
+++ Sparse Mediation/auto_scripts.sh
@@ -0,0 +1,13 @@
+#PBS -l nodes=1:ppn=4
+#PBS -q batch
+#PBS -l walltime=72:00:00
+
+for i in {1..50}
+do
+   echo '#PBS -l nodes=1:ppn=4' >> "run_4_$i.sh"
+   echo '#PBS -q batch' >> "run_4_$i.sh"
+   echo '#PBS -l walltime=72:00:00' >> "run_4_$i.sh"
+   echo 'module load R/3.5.1' >> "run_4_$i.sh"
+   echo "Rscript sparse_qtl_scan.R 4 972 $i 98" >> "run_4_$i.sh"
+   qsub "run_4_$i.sh"
+done
