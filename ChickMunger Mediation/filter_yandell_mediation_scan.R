c80078ce1a5a85c83262fce46ca60f65dc369dbe
duytpm16 <39827376+duytpm16@users.noreply.github.com>
2018-10-29 13:25:29 -0400

    Update filter_yandell_mediation_scan.R

--- ChickMunger Mediation/filter_yandell_mediation_scan.R
+++ ChickMunger Mediation/filter_yandell_mediation_scan.R
@@ -175,7 +175,7 @@ file_name <- gsub('gene','mrna', file_name)
 
 
 if(!is.null(chunk_number)){
-   saveRDS(mediation_results, paste0('attie_islet_',file_name[1],'_filter_yandell_intermediate_',file_name[2],'_chunk_',chunk_number,'.rds'))
+   saveRDS(new_mediation_df, paste0('attie_islet_',file_name[1],'_filter_yandell_intermediate_',file_name[2],'_chunk_',chunk_number,'.rds'))
 }else{
-   saveRDS(mediation_results, paste0('attie_islet_',file_name[1],'_filter_yandell_intermediate_',file_name[2],'.rds'))
+   saveRDS(new_mediation_df, paste0('attie_islet_',file_name[1],'_filter_yandell_intermediate_',file_name[2],'.rds'))
 }
