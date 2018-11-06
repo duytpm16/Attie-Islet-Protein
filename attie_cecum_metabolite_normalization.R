ce31b9f6dc1cd69e7d57ced362b4b88a4abf9ae9
duytpm16 <39827376+duytpm16@users.noreply.github.com>
2018-11-06 10:56:30 -0500

    Update attie_cecum_metabolite_normalization.R

--- attie_cecum_metabolite_normalization.R
+++ attie_cecum_metabolite_normalization.R
@@ -1,23 +1,27 @@
-### This script takes in the raw cecum metabolite data file and normalizes 
+####################################################################################################################################
+#  This script takes in the raw cecum metabolite data file and normalizes 
 #     the data by imputing missing values using pca and comBat normalization.
 #     Additionally, a samples dataframe is created by reading two additional files below.
 #
-### Input:
+# 
+#  Input:
 #     1.) Prefix name to save the output files below.
 #     2.) Raw Attie cecum metabolite file: FilterdbyR_DOCecumMetbolites_BatchandCovariatesAppended.txt
 #     3.) Attie sample annotation file: attie_DO_sample_annot.txt
 #     4.) Sample's Chr M and Y info file: "attie_sample_info_ChrM_Y.csv"
 #
-### Output:
+#
+#  Output:
 #     1.) Filtered raw data file as rds file
 #     2.) Normalized cecum metabolite levels by pca and comBat normaliziation method as a matrix in  rds file
 #     3.) Normalized ranked z data as rds file
 #     4.) New Sample annotation dataframe as rds file
 #
-### Author: Duy Pham, most of the codes were taken from Dan Gatti's script
-### Date:   July 10, 2018
-### E-mail: duy.pham@jax.org
-#####################################################################
+#
+#  Author: Duy Pham
+#  Date:   July 10, 2018
+#  E-mail: duy.pham@jax.org
+####################################################################################################################################
 
 
 
@@ -46,11 +50,23 @@ samples <- read.table("~/Desktop/Attie Final/attie_DO_sample_annot.txt", header 
 chr_m_y <- read.csv("~/Desktop/Attie Final/attie_sample_info_ChrM_Y.csv") 
 
 
+
+
+
+
+
+
 ### Variables to store the data
 raw_file <- paste0(prefix,"_filtered_raw.rds")
 norm_file <- paste0(prefix,"_normalized.rds")
 norm_rz_file <- paste0(prefix,"_rZ_normalized.rds")
 samples_file <-  paste0(prefix, "_samples_annot.rds")
+
+
+
+
+
+
 
 
 
@@ -67,6 +83,12 @@ colnames(samples) <- gsub('_','.',colnames(samples))
 
 
 
+
+
+
+
+
+
 ### Preparing samples dataframe
 #   First, merge columns that are not metabolite to samples data.frame.
 #   Next merge unique columns in chr_m_y dataframe to samples dataframe.
@@ -80,6 +102,11 @@ colnames(samples) <- gsub('_','.',colnames(samples))
 colnames(samples)[grep('Mouse.ID',colnames(samples), ignore.case = TRUE)] <- 'mouse.id'
 
 
+
+
+
+
+
 ### Removing columns that are not phenotypes
 #     Remove non-phenotype columns
 #       Initial dimensions: 366 x 507
@@ -90,8 +117,17 @@ raw <- raw[,!(colnames(raw) %in% c("Mouse.ID","DOwave","birthdate","sex","sac.da
 
 
 
+
+
+
+
 ### 0 duplicates
 sum(duplicated(rownames(raw)))
+
+
+
+
+
 
 
 
@@ -109,8 +145,21 @@ samples = samples[keep,]
 rownames(samples) <- rownames(raw)
 
 
+
+
+
+
+
+
+
 ### Log transformation of the cecum metabolite
 data.log = log(raw)
+
+
+
+
+
+
 
 
 
@@ -168,6 +217,12 @@ if(sum(is.na(raw) > 0)){
 }
 
 
+
+
+
+
+
+
 ### Rank Z of normalized data.
 rankZ = function(x) {
   x = rank(x, na.last = "keep", ties.method = "average") / (sum(!is.na(x)) + 1)
@@ -179,6 +234,12 @@ data.rz = data.log
 for(i in 1:ncol(data.rz)) {
   data.rz[,i] = rankZ(data.rz[,i])
 }
+
+
+
+
+
+
 
 
 
