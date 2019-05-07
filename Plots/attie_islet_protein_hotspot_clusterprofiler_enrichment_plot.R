8e07ad0b013aab7850228117dbdf1eebbbe43098
duytpm16 <39827376+duytpm16@users.noreply.github.com>
2019-05-07 17:17:04 -0400

    Update attie_islet_protein_hotspot_clusterprofiler_enrichment_plot.R

--- Plots/attie_islet_protein_hotspot_clusterprofiler_enrichment_plot.R
+++ Plots/attie_islet_protein_hotspot_clusterprofiler_enrichment_plot.R
@@ -1,6 +1,6 @@
 library(clusterProfiler)
 library(ggplot2)
-
+library(ggraph)
 
 
 
@@ -16,12 +16,13 @@ load("~/Desktop/Attie Mass Spectrometry/Islet/Proteins/Version 2/attie_islet_pro
 cp_data <- 'chr_2_164_CC'
 cp <- get(cp_data)
 n <- nrow(cp)
-plot_cp <- cnetplot(cp, showCategory = nrow(cp), layout = 'mds', colorEdge = TRUE, node_label = TRUE)
+plot_cp <- cnetplot(cp, showCategory = nrow(cp), layout = 'nicely', colorEdge = TRUE, node_label = FALSE)
 data <- plot_cp$data
 data$color <- c(rep('cadetblue1', n), rep('darkseagreen1', nrow(plot_cp$data) - n))
 plot_cp$data <- data
 plot_cp + theme(legend.position = 'none') +
-  ggtitle('Chromsome 2 Hotspot GO - Cellular Component') + theme(plot.title = element_text(hjust = 0.5))
+  ggtitle('Chromsome 2 Hotspot GO - Cellular Component') + theme(plot.title = element_text(hjust = 0.5, size = 20)) +
+  geom_node_text(data = data, aes(x = x, y = y + .05), label = data$name, size = c(7,7,7,5,5,5,5,5,5))
 
 
 
@@ -36,12 +37,14 @@ plot_cp + theme(legend.position = 'none') +
 cp_data <- 'chr_4_14_BP'
 cp <- get(cp_data)
 n <- nrow(cp)
-plot_cp <- cnetplot(cp, showCategory = nrow(cp), layout = 'mds', colorEdge = TRUE, node_label = TRUE)
+plot_cp <- cnetplot(cp, showCategory = nrow(cp), layout = 'randomly', colorEdge = TRUE, node_label = FALSE)
 data <- plot_cp$data
 data$color <- c(rep('cadetblue1', n), rep('darkseagreen1', nrow(plot_cp$data) - n))
 plot_cp$data <- data
 plot_cp + theme(legend.position = 'none') +
-  ggtitle('Chromsome 4 14 Hotspot GO - Biological Processes') + theme(plot.title = element_text(hjust = 0.5))
+  ggtitle('Chromsome 4 14 Hotspot GO - Biological Processes') + 
+  theme(plot.title = element_text(hjust = 0.5, size = 20)) + 
+  geom_node_text(data = data, aes(x = x, y = y), label = data$name, size = c(7,7,5,5,5,5,5))
 
 
 
@@ -49,37 +52,47 @@ plot_cp + theme(legend.position = 'none') +
 cp_data <- 'chr_4_14_CC'
 cp <- get(cp_data)
 n <- nrow(cp)
-plot_cp <- cnetplot(cp, showCategory = nrow(cp), layout = 'mds', colorEdge = TRUE, node_label = TRUE)
+plot_cp <- cnetplot(cp, showCategory = nrow(cp), layout = 'sugiyama', colorEdge = TRUE, node_label = FALSE)
 data <- plot_cp$data
 data$color <- c(rep('cadetblue1', n), rep('darkseagreen1', nrow(plot_cp$data) - n))
 plot_cp$data <- data
 plot_cp + theme(legend.position = 'none') +
-  ggtitle('Chromsome 4 14 Hotspot GO - Cellular Component') + theme(plot.title = element_text(hjust = 0.5))
-
+  ggtitle('Chromsome 4 14 Hotspot GO - Cellular Component') + 
+  theme(plot.title = element_text(hjust = 0.5, size = 20)) +
+  geom_node_text(data = data, aes(x = x + c(.3,.3,.1,rep(.15,11)), y = y - c(-.02,.05,.24,rep(0,11))), label = data$name,
+                 size = c(5,5,5,rep(5,11))) + 
+  coord_flip()
 
 
 
 cp_data <- 'chr_4_14_MF'
 cp <- get(cp_data)
+cp@result <- cp@result[-4,]
 n <- nrow(cp)
 plot_cp <- cnetplot(cp, showCategory = nrow(cp), layout = 'mds', colorEdge = TRUE, node_label = TRUE)
 data <- plot_cp$data
 data$color <- c(rep('cadetblue1', n), rep('darkseagreen1', nrow(plot_cp$data) - n))
 plot_cp$data <- data
 plot_cp + theme(legend.position = 'none') +
-  ggtitle('Chromsome 4 14 Hotspot GO - Molecular Function') + theme(plot.title = element_text(hjust = 0.5))
+  ggtitle('Chromsome 4 14 Hotspot GO - Molecular Function') + theme(plot.title = element_text(hjust = 0.5, size = 20))
+
+
+
 
 
 
 cp_data <- 'chr_4_14_KEGG'
 cp <- get(cp_data)
 n <- nrow(cp)
-plot_cp <- cnetplot(cp, showCategory = nrow(cp), layout = 'mds', colorEdge = TRUE, node_label = TRUE)
+plot_cp <- cnetplot(cp, showCategory = nrow(cp), layout = 'kk', colorEdge = TRUE, node_label = FALSE)
 data <- plot_cp$data
 data$color <- c(rep('cadetblue1', n), rep('darkseagreen1', nrow(plot_cp$data) - n))
 plot_cp$data <- data
 plot_cp + theme(legend.position = 'none') +
-  ggtitle('Chromsome 4 14 Hotspot KEGG Pathway') + theme(plot.title = element_text(hjust = 0.5))
+  ggtitle('Chromsome 4 14 Hotspot KEGG Pathway') + theme(plot.title = element_text(hjust = 0.5, size = 20)) +
+  geom_node_text(data  = data, aes(x = x, y = y), 
+                 label = data$name,
+                 size  = c(7,5,5,5,5,5))
 
 
 
@@ -103,12 +116,16 @@ plot_cp + theme(legend.position = 'none') +
 cp_data <- 'chr_5_139_BP'
 cp <- get(cp_data)
 n <- nrow(cp)
-plot_cp <- cnetplot(cp, showCategory = nrow(cp), layout = 'mds', colorEdge = TRUE, node_label = TRUE)
+plot_cp <- cnetplot(cp, showCategory = nrow(cp), layout = 'mds', colorEdge = TRUE, node_label = FALSE)
 data <- plot_cp$data
 data$color <- c(rep('cadetblue1', n), rep('darkseagreen1', nrow(plot_cp$data) - n))
 plot_cp$data <- data
 plot_cp + theme(legend.position = 'none') +
-  ggtitle('Chromsome 5 139 Hotspot GO - Biological Processes') + theme(plot.title = element_text(hjust = 0.5))
+  ggtitle('Chromsome 5 139 Hotspot GO - Biological Processes') + 
+  theme(plot.title = element_text(hjust = 0.5, size = 20)) + 
+  geom_node_text(data  = data, aes(x = x, y = y), 
+                 label = data$name,
+                 size  = c(rep(5,8), rep(4,26)), repel = TRUE)
 
 
 
@@ -121,7 +138,8 @@ data <- plot_cp$data
 data$color <- c(rep('cadetblue1', n), rep('darkseagreen1', nrow(plot_cp$data) - n))
 plot_cp$data <- data
 plot_cp + theme(legend.position = 'none') +
-  ggtitle('Chromsome 5 139 Hotspot GO - Cellular Component') + theme(plot.title = element_text(hjust = 0.5))
+  ggtitle('Chromsome 5 139 Hotspot GO - Cellular Component') +
+  theme(plot.title = element_text(hjust = 0.5, size = 20))
 
 
 
@@ -145,15 +163,16 @@ plot_cp + theme(legend.position = 'none') +
 cp_data <- 'chr_5_146_CC'
 cp <- get(cp_data)
 n <- nrow(cp)
-plot_cp <- cnetplot(cp, showCategory = nrow(cp), layout = 'mds', colorEdge = TRUE, node_label = TRUE)
+plot_cp <- cnetplot(cp, showCategory = nrow(cp), layout = 'kk', colorEdge = TRUE, node_label = FALSE)
 data <- plot_cp$data
 data$color <- c(rep('cadetblue1', n), rep('darkseagreen1', nrow(plot_cp$data) - n))
 plot_cp$data <- data
 plot_cp + theme(legend.position = 'none') +
-  ggtitle('Chromsome 5 146 Hotspot GO - Cellular Component') + theme(plot.title = element_text(hjust = 0.5))
-
-
-
+  ggtitle('Chromsome 5 146 Hotspot GO - Cellular Component') + 
+  theme(plot.title = element_text(hjust = 0.5, size = 20)) + 
+  geom_node_text(data  = data, aes(x = x, y = y), 
+                 label = data$name,
+                 size  = c(rep(7,2), rep(5,4)), repel = TRUE)
 
 
 
@@ -181,7 +200,8 @@ data <- plot_cp$data
 data$color <- c(rep('cadetblue1', n), rep('darkseagreen1', nrow(plot_cp$data) - n))
 plot_cp$data <- data
 plot_cp + theme(legend.position = 'none') +
-  ggtitle('Chromsome 7 82 Hotspot GO - Biological Processes') + theme(plot.title = element_text(hjust = 0.5))
+  ggtitle('Chromsome 7 82 Hotspot GO - Biological Processes') + 
+  theme(plot.title = element_text(hjust = 0.5, size = 20))
 
 
 
@@ -194,7 +214,8 @@ data <- plot_cp$data
 data$color <- c(rep('cadetblue1', n), rep('darkseagreen1', nrow(plot_cp$data) - n))
 plot_cp$data <- data
 plot_cp + theme(legend.position = 'none') +
-  ggtitle('Chromsome 7 45 Hotspot GO - Cellular Component') + theme(plot.title = element_text(hjust = 0.5))
+  ggtitle('Chromsome 7 45 Hotspot GO - Cellular Component') + 
+  theme(plot.title = element_text(hjust = 0.5, size = 20))
 
 
 
@@ -202,11 +223,12 @@ plot_cp + theme(legend.position = 'none') +
 cp_data <- 'chr_7_45_MF'
 cp <- get(cp_data)
 n <- nrow(cp)
-plot_cp <- cnetplot(cp, showCategory = nrow(cp), layout = 'mds', colorEdge = TRUE, node_label = TRUE)
+plot_cp <- cnetplot(cp, showCategory = nrow(cp), layout = 'kk', colorEdge = TRUE, node_label = TRUE)
 data <- plot_cp$data
 data$color <- c(rep('cadetblue1', n), rep('darkseagreen1', nrow(plot_cp$data) - n))
 plot_cp$data <- data
-plot_cp + ggtitle('Chromsome 7 45 Hotspot GO - Molecular Function') + theme(plot.title = element_text(hjust = 0.5), legend.position = 'none')
+plot_cp + ggtitle('Chromsome 7 45 Hotspot GO - Molecular Function') + 
+  theme(plot.title = element_text(hjust = 0.5, size = 20), legend.position = 'none')
 
 
 
@@ -216,11 +238,12 @@ plot_cp + ggtitle('Chromsome 7 45 Hotspot GO - Molecular Function') + theme(plot
 cp_data <- 'chr_7_45_KEGG'
 cp <- get(cp_data)
 n <- nrow(cp)
-plot_cp <- cnetplot(cp, showCategory = nrow(cp), layout = 'mds', colorEdge = TRUE, node_label = TRUE)
+plot_cp <- cnetplot(cp, showCategory = nrow(cp), layout = 'kk', colorEdge = TRUE, node_label = TRUE)
 data <- plot_cp$data
 data$color <- c(rep('cadetblue1', n), rep('darkseagreen1', nrow(plot_cp$data) - n))
 plot_cp$data <- data
-plot_cp + ggtitle('Chromsome 7 45 Hotspot KEGG Pathway') + theme(plot.title = element_text(hjust = 0.5), legend.position = 'none')
+plot_cp + ggtitle('Chromsome 7 45 Hotspot KEGG Pathway') + 
+  theme(plot.title = element_text(hjust = 0.5, size = 20), legend.position = 'none')
 
 
 
@@ -246,12 +269,13 @@ plot_cp + ggtitle('Chromsome 7 45 Hotspot KEGG Pathway') + theme(plot.title = el
 cp_data <- 'chr_7_82_CC'
 cp <- get(cp_data)
 n <- nrow(cp)
-plot_cp <- cnetplot(cp, showCategory = nrow(cp), layout = 'mds', colorEdge = TRUE, node_label = FALSE)
+plot_cp <- cnetplot(cp, showCategory = nrow(cp), layout = 'nicely', colorEdge = TRUE, node_label = FALSE)
 data <- plot_cp$data
 data$color <- c(rep('cadetblue1', n), rep('darkseagreen1', nrow(plot_cp$data) - n))
 plot_cp$data <- data
 plot_cp + geom_node_text(aes_(x = data$x, y = data$y + c(rep(.03, 14), rep(0, nrow(data) - 14))), label = data$name, repel = TRUE) + theme(legend.position = 'none') +
-  ggtitle('Chromsome 7 82 Hotspot GO - Cellular Component') + theme(plot.title = element_text(hjust = 0.5))
+  ggtitle('Chromsome 7 82 Hotspot GO - Cellular Component') + 
+  theme(plot.title = element_text(hjust = 0.5, size = 20))
 
 
 
@@ -259,11 +283,12 @@ plot_cp + geom_node_text(aes_(x = data$x, y = data$y + c(rep(.03, 14), rep(0, nr
 cp_data <- 'chr_7_82_MF'
 cp <- get(cp_data)
 n <- nrow(cp)
-plot_cp <- cnetplot(cp, showCategory = nrow(cp), layout = 'mds', colorEdge = TRUE, node_label = TRUE)
+plot_cp <- cnetplot(cp, showCategory = nrow(cp), layout = 'nicely', colorEdge = TRUE, node_label = TRUE)
 data <- plot_cp$data
 data$color <- c(rep('cadetblue1', n), rep('darkseagreen1', nrow(plot_cp$data) - n))
 plot_cp$data <- data
-plot_cp + ggtitle('Chromsome 7 82 Hotspot GO - Molecular Function') + theme(plot.title = element_text(hjust = 0.5), legend.position = 'none')
+plot_cp + ggtitle('Chromsome 7 82 Hotspot GO - Molecular Function') + 
+  theme(plot.title = element_text(hjust = 0.5, size = 20), legend.position = 'none')
 
 
 
@@ -292,12 +317,13 @@ plot_cp + ggtitle('Chromsome 7 82 Hotspot KEGG Pathway') + theme(plot.title = el
 cp_data <- 'chr_10_41_BP'
 cp <- get(cp_data)
 n <- nrow(cp)
-plot_cp <- cnetplot(cp, showCategory = nrow(cp), layout = 'mds', colorEdge = TRUE, node_label = TRUE)
+plot_cp <- cnetplot(cp, showCategory = nrow(cp), layout = 'nicely', colorEdge = TRUE, node_label = TRUE)
 data <- plot_cp$data
 data$color <- c(rep('cadetblue1', n), rep('darkseagreen1', nrow(plot_cp$data) - n))
 plot_cp$data <- data
 plot_cp + theme(legend.position = 'none') +
-  ggtitle('Chromsome 10 Hotspot GO - Biological Processes') + theme(plot.title = element_text(hjust = 0.5))
+  ggtitle('Chromsome 10 Hotspot GO - Biological Processes') + 
+  theme(plot.title = element_text(hjust = 0.5, size = 20))
 
 
 
@@ -309,7 +335,8 @@ data <- plot_cp$data
 data$color <- c(rep('cadetblue1', n), rep('darkseagreen1', nrow(plot_cp$data) - n))
 plot_cp$data <- data
 plot_cp + geom_node_text(aes_(x = data$x, y = data$y + c(rep(.03, 14), rep(0, nrow(data) - 14))), label = data$name, repel = TRUE) + theme(legend.position = 'none') +
-  ggtitle('Chromsome 10 Hotspot GO - Cellular Component') + theme(plot.title = element_text(hjust = 0.5))
+  ggtitle('Chromsome 10 Hotspot GO - Cellular Component') +
+  theme(plot.title = element_text(hjust = 0.5, size = 20))
 
 
 
@@ -321,9 +348,8 @@ plot_cp <- cnetplot(cp, showCategory = nrow(cp), layout = 'mds', colorEdge = TRU
 data <- plot_cp$data
 data$color <- c(rep('cadetblue1', n), rep('darkseagreen1', nrow(plot_cp$data) - n))
 plot_cp$data <- data
-plot_cp + ggtitle('Chromsome 10 Hotspot GO - Molecular Function') + theme(plot.title = element_text(hjust = 0.5), legend.position = 'none')
-
-
+plot_cp + ggtitle('Chromsome 10 Hotspot GO - Molecular Function') +
+  theme(plot.title = element_text(hjust = 0.5, size = 20), legend.position = 'none')
 
 
 
