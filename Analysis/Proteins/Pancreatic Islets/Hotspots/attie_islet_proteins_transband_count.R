a6e13d2db595381d5e521870af8e4d4065599da4
duytpm16 <39827376+duytpm16@users.noreply.github.com>
2019-09-17 17:03:52 -0400

    Update attie_islet_proteins_transband_count.R

--- Analysis/Proteins/Pancreatic Islets/Hotspots/attie_islet_proteins_transband_count.R
+++ Analysis/Proteins/Pancreatic Islets/Hotspots/attie_islet_proteins_transband_count.R
@@ -34,7 +34,7 @@ peaks   <- get(dataset)$lod.peaks$additive
 #           slide   - Mbp to slide across genome
 #           window  - Width of window in Mbps
 #
-#       Note*: Warning is due to 'X' chromosome conversion to numeric. Need this to reorder 'chr' column.
+#       Note*: Warning is due to 'X' chromosome conversion to numeric. Need this to reorder 'chr' column (line 51).
 transband_count <- function(markers, peaks, slide, window){
 
     counts_df <- markers %>% 
