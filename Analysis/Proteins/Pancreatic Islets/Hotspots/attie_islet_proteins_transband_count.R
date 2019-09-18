cec2249093d7ae8c157ea51dc5bad9de18ad74fa
duytpm16 <39827376+duytpm16@users.noreply.github.com>
2019-09-18 03:47:35 -0400

    Update attie_islet_proteins_transband_count.R

--- Analysis/Proteins/Pancreatic Islets/Hotspots/attie_islet_proteins_transband_count.R
+++ Analysis/Proteins/Pancreatic Islets/Hotspots/attie_islet_proteins_transband_count.R
@@ -37,20 +37,18 @@ peaks   <- get(dataset)$lod.peaks$additive
 #       Note*: 
 #           Warning is due to 'X' chromosome conversion to numeric. Need this to reorder 'chr' column (line 52).
 transband_count <- function(markers, peaks, slide, window){
-
-    counts_df <- markers %>% 
-                  group_by(chr) %>% 
-                  summarise(min = plyr::round_any(min(pos), accuracy = 1, f = floor),
-                            max = plyr::round_any(max(pos), accuracy = window, f = ceiling),
-                            len = length(seq(min, max, slide))) %>%
-                  uncount(len) %>%
-                  group_by(chr) %>%
-                  mutate(pos = seq(unique(min), unique(max), slide), pos = ((pos + window) + pos)/2) %>%
-                  select(-min, -max) %>%
-                  group_by(chr, pos) %>%
-                  mutate(counts = sum(peaks$qtl.chr == chr & abs(peaks$qtl.pos - pos) <= window /2)) %>%
-                  arrange(as.numeric(chr), chr)
-    counts_df             
+  
+      markers %>% 
+          group_by(chr) %>% 
+          summarise(min = plyr::round_any(min(pos), accuracy = 1, f = floor),
+                    max = plyr::round_any(max(pos), accuracy = window, f = ceiling),
+                    len = length(seq(min, max, slide))) %>%
+          uncount(len) %>% group_by(chr) %>%
+          mutate(pos = seq(unique(min), unique(max), slide), pos = ((pos + window) + pos)/2) %>%
+          select(-min, -max) %>% group_by(chr, pos) %>%
+          mutate(counts = sum(peaks$qtl.chr == chr & abs(peaks$qtl.pos - pos) <= window /2)) %>%
+          arrange(as.numeric(chr), chr)
+  
 }      
 
 
@@ -65,22 +63,22 @@ transband_count <- function(markers, peaks, slide, window){
 
 
 
-### Count number of qtls with LOD 6 in 4Mbp windows and 1 Mbp step across genome
+### Count number of qtls with LOD 6 in 4Mbp windows and 1 Mbp step across genome. Finally select windows with highest counts
 peaks6 <- peaks %>% filter(!cis & lod > 6)
 counts_lod6   <- transband_count(marker = markers, peaks = peaks6, slide = 1, window = 4)
 counts_lod6   <- counts_lod6 %>% 
-                     filter(counts >= 45) %>% 
-                     filter(chr %in% c('2', '4', '5', '7', '10', '14')) %>%
-                     filter(pos %in% c(164, 14, 139, 146, 45, 82, 41, 101))
+  filter(counts >= 45) %>% 
+  filter(chr %in% c('2', '4', '5', '7', '10', '14')) %>%
+  filter(pos %in% c(164, 14, 139, 146, 45, 82, 41, 101))
 
 
-### Count number of qtls with LOD 7.3 in 4Mbp windows and 1 Mbp step across genome
+### Count number of qtls with LOD 7.3 in 4Mbp windows and 1 Mbp step across genome. Finally select windows with highest counts
 peaks7.3 <- peaks %>% filter(!cis & lod > 7.3)
 counts_lod7.3 <- transband_count(marker = markers, peaks = peaks7.3, slide = 1, window = 4)
 counts_lod7.3 <- counts_lod7.3 %>% 
-                    filter(counts >= 15) %>% 
-                    filter(chr %in% c('2', '4', '5', '7')) %>%
-                    filter(pos %in% c(163, 13, 139, 146, 45, 82))
+  filter(counts >= 15) %>% 
+  filter(chr %in% c('2', '4', '5', '7')) %>%
+  filter(pos %in% c(163, 13, 139, 146, 45, 82))
 
 
 
