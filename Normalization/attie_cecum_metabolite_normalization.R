821ea8bae3ea49f5cde412fb843f4d4759251562
Duy T. Pham <39827376+duytpm16@users.noreply.github.com>
2019-10-09 11:49:44 -0400

    Update attie_cecum_metabolite_normalization.R

--- Normalization/attie_cecum_metabolite_normalization.R
+++ Normalization/attie_cecum_metabolite_normalization.R
@@ -1,52 +1,45 @@
-#############################################################################################################################################
-#
-#  This script takes in the raw plasma metabolite data file and normalizes 
+####################################################################################################################################
+#  This script takes in the raw cecum metabolite data file and normalizes 
 #     the data by imputing missing values using pca and comBat normalization.
 #     Additionally, a samples dataframe is created by reading two additional files below.
 #
-#
+# 
 #  Input:
-#     1.) Prefix name to save the output files below.
-#     2.) raw Attie plasma metabolite file: FilterdbyR_DOPlasmaMetbolites_BatchandCovariatesAppended.txt
-#     3.) Attie sample annotation file: attie_DO_sample_annot.txt
-#     4.) Sample's Chr M and Y info file: "attie_sample_info_ChrM_Y.csv"
+#     1.) Raw Attie cecum metabolite file: FilterdbyR_DOCecumMetbolites_BatchandCovariatesAppended.txt
+#     2.) Attie sample annotation file: attie_DO_sample_annot.txt
+#     3.) Sample's Chr M and Y info file: "attie_sample_info_ChrM_Y.csv"
 #
 #
 #  Output:
-#     1.) Filtered raw data file as rds file
-#     2.) Normalized plasma metabolite levels by pca and comBat normaliziation method as a matrix in  rds file
-#     3.) Normalized ranked z data as rds file
-#     4.) New Sample annotation dataframe as rds file
-#
+#     1.) Filtered raw data file 
+#     2.) Normalized cecum metabolite levels by pca and comBat normaliziation method as a matrix
+#     3.) Normalized ranked z data 
+#     4.) New Sample annotation dataframe 
+#     5.) Covariate matrix
 #
 #  Author: Duy Pham
 #  Date:   July 10, 2018
 #  E-mail: duy.pham@jax.org
-#############################################################################################################################################
-
-
-
-### Install required packages
-# source("https://bioconductor.org/biocLite.R")
-# bioclite(c('sva','pcaMethods'))
-# install.packages('tidyverse')
+####################################################################################################################################
 
 ### Load required libraries
+options(stringsAsFactors = FALSE)
 library(sva)
 library(pcaMethods)
 
 
-### Options
-options(stringsAsFactors = FALSE)
 
 
 
-### Read in the raw plasma metabolite data, sample annotation, and Chr M and Y files.
-#     plasma_metbaolite_raw: 380 x 342
+
+
+
+
+### Read in the raw cecum metabolite data, sample annotation, and Chr M and Y files.
+#     Cecum metabolites raw: 366 x 507
 #     samples: 500 x 7
 #     chr: 498 x 5
-prefix   <- '~/Desktop/Attie Mass Spectrometry/Metabolites/Plasma/attie_plasma_metabolite'
-raw     <- read.delim("~/Desktop/Attie Mass Spectrometry/Metabolites/Plasma/FilterdbyR_DOPlasmaMetbolites_BatchandCovariatesAppended.txt")
+raw <- read.delim("~/Desktop/Attie Mass Spectrometry/Metabolites/Cecum/FilterdbyR_DOCecumMetbolites_BatchandCovariatesAppended.txt")
 samples <- read.table("~/Desktop/Attie Mass Spectrometry/Sample Info/attie_DO_sample_annot.txt", header = TRUE ,sep = "\t")
 chr_m_y <- read.csv("~/Desktop/Attie Mass Spectrometry/Sample Info/attie_sample_info_ChrM_Y.csv") 
 
@@ -58,31 +51,18 @@ chr_m_y <- read.csv("~/Desktop/Attie Mass Spectrometry/Sample Info/attie_sample_
 
 
 
-### Variables to store the data
-raw_file     <- paste0(prefix,"_filtered_raw.rds")
-norm_file    <- paste0(prefix,"_normalized.rds")
-norm_rz_file <- paste0(prefix,"_rZ_normalized.rds")
-samples_file <- paste0(prefix, "_samples_annot.rds")
-
-
-
-
-
-
-
 
 
 ### Fixing the name of two columns in the samples dataframe to match a data dictionary that will be used later in other scripts
-colnames(samples)[grep('wave',colnames(samples), ignore.case = TRUE)]  <- 'DOwave'
+colnames(samples)[grep('wave',colnames(samples), ignore.case = TRUE)] <- 'DOwave'
 colnames(samples)[grep('batch',colnames(samples), ignore.case = TRUE)] <- 'batch'
-colnames(raw)[grep('\\wave$', colnames(raw), ignore.case = TRUE)]  <- 'DOwave'
-colnames(raw)[grep('\\batch$', colnames(raw), ignore.case = TRUE)] <- 'batch'
+colnames(raw)[grep('wave', colnames(raw), ignore.case = TRUE)] <- 'DOwave'
+colnames(raw)[grep('batch', colnames(raw), ignore.case = TRUE)] <- 'batch'
 
-raw$Mouse.ID      <- gsub('-', '', raw$Mouse.ID)
-samples$Mouse.ID  <- gsub('-', '', samples$Mouse.ID)
-chr_m_y$Mouse.ID  <- gsub('-', '', chr_m_y$Mouse.ID)
+raw$Mouse.ID <- gsub('-', '', raw$Mouse.ID)
+samples$Mouse.ID <- gsub('-', '', samples$Mouse.ID)
+chr_m_y$Mouse.ID <- gsub('-', '', chr_m_y$Mouse.ID)
 colnames(samples) <- gsub('_','.',colnames(samples))
-
 
 
 
@@ -95,12 +75,12 @@ colnames(samples) <- gsub('_','.',colnames(samples))
 ### Preparing samples dataframe
 #   First, merge columns that are not metabolite to samples data.frame.
 #   Next merge unique columns in chr_m_y dataframe to samples dataframe.
-#       samples: 
+#       samples: 366 x 11
 samples <- merge(samples, raw[,c("Mouse.ID","DOwave","birthdate","sex","sac.date","coat.color","batch")], 
                  by = "Mouse.ID")
 samples <- merge(samples, chr_m_y[,c('Mouse.ID','generation','chrM','chrY')], by = "Mouse.ID")
-samples <- samples[,-grep('\\.x$',colnames(samples))]
-colnames(samples) <- gsub('\\.y$','',colnames(samples))
+samples <- samples[,-grep('\\.y$',colnames(samples))]
+colnames(samples) <- gsub('\\.x$','',colnames(samples))
 colnames(samples) <- gsub('_','.',colnames(samples))
 colnames(samples)[grep('Mouse.ID',colnames(samples), ignore.case = TRUE)] <- 'mouse.id'
 
@@ -110,11 +90,10 @@ colnames(samples)[grep('Mouse.ID',colnames(samples), ignore.case = TRUE)] <- 'mo
 
 
 
-
-
-### Removing column that are not metabolite data
-#     Inital dimensions: 380 x 342
-#     After dimensions: 380 x 335 without Mouse.ID
+### Removing columns that are not phenotypes
+#     Remove non-phenotype columns
+#       Initial dimensions: 366 x 507
+#       After dimensions: 366 x 500
 raw <- raw[grep('DO', raw$Mouse.ID),]
 rownames(raw) <- raw$Mouse.ID
 raw <- raw[,!(colnames(raw) %in% c("Mouse.ID","DOwave","birthdate","sex","sac.date","coat.color","batch"))]
@@ -125,19 +104,28 @@ raw <- raw[,!(colnames(raw) %in% c("Mouse.ID","DOwave","birthdate","sex","sac.da
 
 
 
+### 0 duplicates
+sum(duplicated(rownames(raw)))
+
+
+
+
+
+
 
 
 ### Remove samples with more than 25% missing data.
-#     Initial Dimensions: 380 x 336
-#     After Dimensions: 368 x 336
+#     Initial dimension: 366 x 500
+#     After dimensiom: 364 x 500
 prop.missing = rowMeans(is.na(raw))
 sum(prop.missing > 0.25)
 rownames(raw)[prop.missing > 0.25]
 
+
 keep = which(prop.missing < 0.25)
 raw = raw[keep,]
 samples = samples[keep,]
-rownames(samples) <- samples$mouse.id
+rownames(samples) <- rownames(raw)
 
 
 
@@ -147,7 +135,7 @@ rownames(samples) <- samples$mouse.id
 
 
 
-### Log transformation of the plasma metabolite abundance
+### Log transformation of the cecum metabolite
 data.log = log(raw)
 
 
@@ -160,9 +148,9 @@ data.log = log(raw)
 
 ### Set up batch and model for comBat
 samples$sex  = factor(samples$sex)
-samples$DOwave = factor(samples$DOwave)
 mod = model.matrix(~sex, data = samples)
 batch = samples$batch
+
 
 
 ### If there are no missing data, just combat normalize.
@@ -199,7 +187,6 @@ if(sum(is.na(raw) > 0)){
       iter = iter + 1    
       
     }else{
-      
       data.cb[miss] = NA
       data.log = data.cb
       break
@@ -227,11 +214,8 @@ rankZ = function(x) {
 data.rz = data.log
 
 for(i in 1:ncol(data.rz)) {
-  data.rz[,i] = rankZ(data.rz[,i])
+    data.rz[,i] = rankZ(data.rz[,i])
 }
-
-
-
 
 
 
@@ -260,16 +244,16 @@ covar.info <- data.frame(sample.column = c('sex', 'DOwave', 'batch'),
 
 
 ### QTL viewer format
-dataset.plasma.metabolites <- list(annot.phenotype = data.frame(),
-                                   annot.samples   = as_tibble(samples),
-                                   covar.matrix    = as.matrix(covar),
-                                   covar.info      = as_tibble(covar.info),
-                                   data            = list(norm = as.matrix(data.log),
-                                                          raw  = as.matrix(raw),
-                                                          rz   = as.matrix(data.rz)),
-                                   datatype        = 'phenotype',
-                                   display.name    = 'Attie Plasma Metabolites',
-                                   lod.peaks       = list())
+dataset.cecum.metabolites <- list(annot.phenotype = data.frame(),
+                                  annot.samples   = as_tibble(samples),
+                                  covar.matrix    = as.matrix(covar),
+                                  covar.info      = as_tibble(covar.info),
+                                  data            = list(norm = as.matrix(data.log),
+                                                         raw  = as.matrix(raw),
+                                                         rz   = as.matrix(data.rz)),
+                                  datatype        = 'phenotype',
+                                  display.name    = 'Attie Cecum Metabolites',
+                                  lod.peaks       = list())
 
 
 
@@ -281,9 +265,7 @@ dataset.plasma.metabolites <- list(annot.phenotype = data.frame(),
 
 ### Save
 rm(list = ls()[!grepl('dataset[.]', ls())])
-save(dataset.plasma.metabolites, file = '~/Desktop/Attie Mass Spectrometry/Metabolites/Plasma/attie_plasma_metabolite_viewer.Rdata')
-
-
+save(dataset.cecum.metabolites, file = '~/Desktop/Attie Mass Spectrometry/Metabolites/Cecum/attie_cecum_metabolite_viewer.Rdata')
 
 
 
