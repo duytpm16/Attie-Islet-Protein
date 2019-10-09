09a9674742c8c4d8fc0aa3f4e37041956afd1fe7
Duy T. Pham <39827376+duytpm16@users.noreply.github.com>
2019-10-09 11:46:02 -0400

    Update attie_liver_lipids_normalization.R

--- Normalization/attie_liver_lipids_normalization.R
+++ Normalization/attie_liver_lipids_normalization.R
@@ -7,7 +7,7 @@
 #
 #  Input:
 #     1.) Prefix name to save the output files below.
-#     2.) Raw Attie liver lipids file: FilterdbyR_DOCecumMetbolites_BatchandCovariatesAppended.txt
+#     2.) Raw Attie liver lipids file: 03_January_2018_DO_Liver_Lipidomics_Raw.txt
 #     3.) Attie sample annotation file: attie_DO_sample_annot.txt
 #     4.) Sample's Chr M and Y info file: "attie_sample_info_ChrM_Y.csv"
 #
@@ -36,24 +36,12 @@ library(sva)
 #      liver_lipid_raw: 419 x 1564
 #      samples: 500 x 7
 #      chr_m_y: 498 x 5
-prefix <- '~/Desktop/Attie Final/Lipids/Liver/attie_liver_lipid'
-raw <- read.delim("~/Desktop/Attie Final/Lipids/Liver/03_January_2018_DO_Liver_Lipidomics_Raw.txt")
-samples <- read.delim('~/Desktop/Attie Final/attie_DO_sample_annot.txt')
-chr_m_y <- read.csv("~/Desktop/Attie Final/attie_sample_info_ChrM_Y.csv")
+raw <- read.delim("~/Desktop/Attie Mass Spectrometry/Lipids/Liver/03_January_2018_DO_Liver_Lipidomics_Raw.txt")
+samples <- read.delim('~/Desktop/Attie Mass Spectrometry/Sample Info/attie_DO_sample_annot.txt')
+chr_m_y <- read.csv("~/Desktop/Attie Mass Spectrometry/Sample Info/attie_sample_info_ChrM_Y.csv")
 
 
 
-
-
-
-
-
-
-### Variable names to store data
-raw_file <- paste0(prefix,"_filtered_raw.rds")
-norm_file <- paste0(prefix,"_normalized.rds")
-norm_rz_file <- paste0(prefix,"_rZ_normalized.rds")
-samples_file <-  paste0(prefix, "_samples_annot.rds")
 
 
 
@@ -168,44 +156,44 @@ batch = samples$batch
 ### If there are no missing data, just combat normalize.
 if(sum(is.na(raw) > 0)){
   
-    ### Impute missing data and combat normalization
-    chg = 1e6
-    iter = 1
-    repeat({
+  ### Impute missing data and combat normalization
+  chg = 1e6
+  iter = 1
+  repeat({
+    
+    print(paste("Iteration", iter))
+    
+    # Impute missing data using pca
+    miss = which(is.na(data.log))
+    print(paste(length(miss), "missing points."))
+    
+    pc.data = pca(data.log, method = "bpca", nPcs = 5)
+    data.compl = completeObs(pc.data)
+    
+    # Batch adjust.
+    # ComBat wants the data with variable in rows and samples in columns.
+    data.cb = ComBat(dat = t(data.compl), batch = batch, mod = mod)
+    data.cb = t(data.cb)
+    
+    # Calculate the change.
+    chg = sum((data.compl[miss] - data.cb[miss])^2)
+    print(paste("SS Change:", chg))
+    
+    # Put the missing data back in and impute again.
+    if(chg > 5 & iter < 20) {
       
-      print(paste("Iteration", iter))
+      data.cb[miss] = NA
+      data.log = data.cb
+      iter = iter + 1    
       
-      # Impute missing data using pca
-      miss = which(is.na(data.log))
-      print(paste(length(miss), "missing points."))
+    }else{
       
-      pc.data = pca(data.log, method = "bpca", nPcs = 5)
-      data.compl = completeObs(pc.data)
+      data.cb[miss] = NA
+      data.log = data.cb
+      break
       
-      # Batch adjust.
-      # ComBat wants the data with variable in rows and samples in columns.
-      data.cb = ComBat(dat = t(data.compl), batch = batch, mod = mod)
-      data.cb = t(data.cb)
-      
-      # Calculate the change.
-      chg = sum((data.compl[miss] - data.cb[miss])^2)
-      print(paste("SS Change:", chg))
-      
-      # Put the missing data back in and impute again.
-      if(chg > 5 & iter < 20) {
-        
-        data.cb[miss] = NA
-        data.log = data.cb
-        iter = iter + 1    
-        
-      }else{
-        
-        data.cb[miss] = NA
-        data.log = data.cb
-        break
-        
-      }
-    })
+    }
+  })
 }else{
   data.log = ComBat(dat = t(data.log), batch = batch, mod = mod)
   data.log = t(data.log)
@@ -241,8 +229,47 @@ for(i in 1:ncol(data.rz)) {
 
 
 
-### Saving the data to current working directory
-saveRDS(raw, raw_file)
-saveRDS(data.log, norm_file)
-saveRDS(data.rz, norm_rz_file)
-saveRDS(samples, samples_file)
+
+
+
+### Covariates
+covar <- model.matrix(~ sex + DOwave + batch, data = samples)[,-1,drop = FALSE]
+
+covar.info <- data.frame(sample.column = c('sex', 'DOwave', 'batch'),
+                         covar.column  = c('sexM', 'DOwave', 'batch'),
+                         display.name  = c('Sex', 'DO wave', 'Batch'),
+                         interactive   = c(TRUE, FALSE, FALSE),
+                         primary       = c(TRUE, FALSE, FALSE),
+                         lod.peaks     = c('sex_int', NA, NA))
+
+
+
+
+
+
+
+
+
+### QTL viewer format
+dataset.liver.lipids <- list(annot.phenotype = data.frame(),
+                             annot.samples   = as_tibble(samples),
+                             covar.matrix    = as.matrix(covar),
+                             covar.info      = as_tibble(covar.info),
+                             data            = list(norm = as.matrix(data.log),
+                                                    raw  = as.matrix(raw),
+                                                    rz   = as.matrix(data.rz)),
+                             datatype        = 'phenotype',
+                             display.name    = 'Attie Liver Lipids',
+                             lod.peaks       = list())
+
+
+
+
+
+
+
+
+
+### Save
+rm(list = ls()[!grepl('dataset[.]', ls())])
+save(dataset.liver.lipids, file = '~/Desktop/Attie Mass Spectrometry/Lipids/Liver/attie_liver_lipid_viewer.Rdata')
